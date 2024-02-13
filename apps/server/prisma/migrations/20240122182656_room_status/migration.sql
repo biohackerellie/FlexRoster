dca1ae4689b1ea35440a0a39941635e05e088b82
biohackerellie <epkerns@gmail.com>
2024-02-13 13:19:57 -0700

    bye bye prisma

--- apps/server/prisma/migrations/20240122182656_room_status/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "VerificationToken" ALTER COLUMN "code" SET DEFAULT FLOOR(RANDOM()*1000000),
-ALTER COLUMN "expires" SET DEFAULT CURRENT_DATE + INTERVAL '12 HOUR';
-
--- AlterTable
-ALTER TABLE "classrooms" ADD COLUMN     "available" BOOLEAN NOT NULL DEFAULT true;
--- apps/server/prisma/migrations/20240131170943_cascades/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "classRosters" DROP CONSTRAINT "classRosters_classroomId_fkey";
-
--- AddForeignKey
-ALTER TABLE "classRosters" ADD CONSTRAINT "classRosters_classroomId_fkey" FOREIGN KEY ("classroomId") REFERENCES "classrooms"("id") ON DELETE CASCADE ON UPDATE CASCADE;
