9babb546e373f53826c23fb849cf50bc71632936
biohackerellie <epkerns@gmail.com>
2024-06-03 14:21:31 -0600

    fix: middleware

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -1,71 +1,70 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import NextAuth from "next-auth";
 
-// import NextAuth from "next-auth";
+import authConfig from "@local/auth/auth.config";
 
-// import authConfig from "@local/auth/auth.config";
+const { auth: middleware } = NextAuth(authConfig);
 
-// const { auth: middleware } = NextAuth(authConfig);
+export default middleware((req) => {
+  const response = NextResponse.next();
+  const token = req.auth;
 
-// export default middleware((req) => {
-//   const response = NextResponse.next();
-//   const token = req.auth;
+  const path = req.nextUrl.pathname;
 
-//   const path = req.nextUrl.pathname;
+  if (!token) {
+    if (path.startsWith("/dashboard")) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
 
-//   if (!token) {
-//     if (path.startsWith("/dashboard")) {
-//       return NextResponse.redirect(new URL("/login", req.url));
-//     }
+    return NextResponse.next();
+  }
+  const role = token.user?.roles || "student";
+  const id = token.user?.id || "0";
 
-//     return NextResponse.next();
-//   }
-//   const role = token.user?.roles || "student";
-//   const id = token.user?.id || "0";
+  switch (role) {
+    case "student":
+      if (path === "/dashboard") {
+        return NextResponse.redirect(new URL(`/dashboard/student`, req.url));
+      }
 
-//   switch (role) {
-//     case "student":
-//       if (path === "/dashboard") {
-//         return NextResponse.redirect(new URL(`/dashboard/student`, req.url));
-//       }
+      if (path.startsWith("/dashboard/staff")) {
+        return NextResponse.redirect(new URL(`/dashboard/student`, req.url));
+      }
+      if (path === "/dashboard/admin") {
+        return NextResponse.redirect(new URL(`/dashboard/student`, req.url));
+      }
+      break;
+    case "teacher":
+      if (path === "/dashboard") {
+        return NextResponse.redirect(new URL(`/dashboard/staff`, req.url));
+      }
+      if (path === "/dashboard/student") {
+        return NextResponse.redirect(new URL(`/dashboard/staff/`, req.url));
+      }
+      break;
+    case "admin":
+      if (path === "/dashboard") {
+        return NextResponse.redirect(new URL("/dashboard/staff", req.url));
+      }
 
-//       if (path.startsWith("/dashboard/staff")) {
-//         return NextResponse.redirect(new URL(`/dashboard/student`, req.url));
-//       }
-//       if (path === "/dashboard/admin") {
-//         return NextResponse.redirect(new URL(`/dashboard/student`, req.url));
-//       }
-//       break;
-//     case "teacher":
-//       if (path === "/dashboard") {
-//         return NextResponse.redirect(new URL(`/dashboard/staff`, req.url));
-//       }
-//       if (path === "/dashboard/student") {
-//         return NextResponse.redirect(new URL(`/dashboard/staff/`, req.url));
-//       }
-//       break;
-//     case "admin":
-//       if (path === "/dashboard") {
-//         return NextResponse.redirect(new URL("/dashboard/staff", req.url));
-//       }
+      break;
+    case "secretary":
+      if (path === "/dashboard") {
+        return NextResponse.redirect(new URL("/dashboard/staff", req.url));
+      }
+      if (path === "/dashboard/student") {
+        return NextResponse.redirect(new URL("/dashboard/staff", req.url));
+      }
+      if (path.startsWith("/dashboard/chat")) {
+        return NextResponse.redirect(new URL("/dashboard/staff", req.url));
+      }
 
-//       break;
-//     case "secretary":
-//       if (path === "/dashboard") {
-//         return NextResponse.redirect(new URL("/dashboard/staff", req.url));
-//       }
-//       if (path === "/dashboard/student") {
-//         return NextResponse.redirect(new URL("/dashboard/staff", req.url));
-//       }
-//       if (path.startsWith("/dashboard/chat")) {
-//         return NextResponse.redirect(new URL("/dashboard/staff", req.url));
-//       }
+      break;
+  }
 
-//       break;
-//   }
-
-//   return response;
-// });
+  return response;
+});
 
 export const config = {
   matcher: [
@@ -79,6 +78,6 @@ export const config = {
   ],
 };
 
-export function middleware(req: NextRequest) {
-  return NextResponse.next();
-}
+// export function middleware(req: NextRequest) {
+//   return NextResponse.next();
+// }
