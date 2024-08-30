426ad2b85ece041b67e994f02c0a333bb0be5f6f
biohackerellie <epkerns@gmail.com>
2024-08-30 07:25:27 -0600

    chore: demo

--- apps/client/src/components/signIn.tsx
+++ apps/client/src/components/signIn.tsx
@@ -1,17 +1,25 @@
 import { signIn } from "@local/auth";
 import { Button } from "@local/ui/button";
 
+import { env } from "@/env";
+
 const LoginButton = () => {
   return (
     <div className="flex py-2">
       <form
         action={async () => {
           "use server";
-
-          await signIn("azure-ad", {
-            redirect: true,
-            redirectTo: "/dashboard",
-          });
+          if (env.NEXT_PUBLIC_DEMO) {
+            await signIn("google", {
+              redirect: true,
+              redirectTo: "/dashboard",
+            });
+          } else {
+            await signIn("azure-ad", {
+              redirect: true,
+              redirectTo: "/dashboard",
+            });
+          }
         }}
       >
         <Button type="submit" variant="outline" className="w-full">
